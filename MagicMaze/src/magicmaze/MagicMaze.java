45ea44f6596e7f262e782720e7cbe58bf6eee106
jonathan <jonathan@Phobos>
2018-03-28 01:20:51 +0200

    Initial Stuff and Playing field

---
+++ MagicMaze/src/magicmaze/MagicMaze.java
@@ -0,0 +1,18 @@
+package magicmaze;
+
+import myGraphics.MainWindow;
+
+/**
+ *
+ * @author cblech
+ */
+public class MagicMaze {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainWindow.open(args);
+    }
+    
+}
